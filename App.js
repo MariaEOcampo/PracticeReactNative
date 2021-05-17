9612c21b76ed6fc8b20c2d58b56387c76b99d588
Maria Emilia Ocampo <maria.ocampo@globant.com>
2021-05-16 23:25:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import imagem from "./assets/simpson-5x11.jpg";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import uploadToAnonynousFilesAsync from "anonymous-files";
 
-export default function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("Permission to acess camera is required");
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    if (Platform.OS === "web") {
+      const remoteUri = await uploadToAnonynousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+  };
+
+  const openShareDialog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Pick an Image!!</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://picsum.photos/200/200",
+          }} //ó {{imagem}} cuando es algo local
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      {/* <Button
+        color="red"
+        title="Press Me"
+        onPress={() => Alert.alert("Hello!!")}
+      /> */}
+
+      {selectedImage ? (
+        <TouchableOpacity onPress={openShareDialog} style={styles.button}>
+          <Text style={styles.button}>Share this image</Text>
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#292929",
   },
+  title: { fontSize: 30, color: "#ffff" },
+  image: { height: 200, width: 200, borderRadius: 90 },
+  button: {
+    backgroundColor: "yellow",
+    padding: 7,
+    marginTop: 5,
+    borderRadius: 5,
+  },
+  buttonText: { color: "#fff" },
 });
+
+export default App;
